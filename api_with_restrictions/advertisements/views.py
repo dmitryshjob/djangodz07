7fda0532476d8ac0e41c393a9b349f1ae6c8f758
Dmitry <dmitryshjod@yandex.ru>
2023-04-13 14:04:26 +0300

    Django7

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.throttling import UserRateThrottle, AnonRateThrottle
+
+from .models import Advertisement
+from .serializers import AdvertisementSerializer
+from .filters import AdvertisementFilter
+from .permissions import IsOwnerOrReadOnly
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = AdvertisementFilter
+    throttle_classes = [UserRateThrottle, AnonRateThrottle]
+
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create", "update", "partial_update", 'destroy']:
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        return []
